60cfa2682c5393ae8e9f8edf20ea4761180ba5f7
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-03-14 20:15:38 -0300

    feat: init BuildScreen

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -4,6 +4,7 @@ import {
   Routes as _Routes,
 } from 'react-router-dom';
 
+import { BuildScreen } from '@screens/Build/BuildScreen';
 import { CodeScreen } from '@screens/Code/CodeScreen';
 import { PlanScreen } from '@screens/Plan/PlanScreen';
 
@@ -15,6 +16,7 @@ export const Routes = () => {
           <Route path={path} key={path} element={<PlanScreen />} />
         ))}
         <Route path="/code" element={<CodeScreen />} />
+        <Route path="/build" element={<BuildScreen />} />
       </_Routes>
     </Router>
   );
