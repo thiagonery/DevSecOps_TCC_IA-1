daceb66f4ee7b19aa0ec3a08f6b2103a11628df1
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-03-24 01:51:36 -0300

    fix: add alias path in axios.ts

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { BACKEND_BASE_URL } from 'src/config/api';
+
+import { BACKEND_BASE_URL } from '@config/api';
 
 export const instance = axios.create({
   baseURL: BACKEND_BASE_URL,
