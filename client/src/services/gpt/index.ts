9fcbe7033398890db61fa8fbc777283c4993dbac
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-03-27 10:35:57 -0300

    chore: create fetchGPTCode

--- client/src/services/gpt/index.ts
+++ client/src/services/gpt/index.ts
@@ -2,5 +2,14 @@ import { instance } from '@services/axios';
 
 export const fetchGPTPlan = (technology: string, dbms: string) =>
   instance({
-    url: `api/v1/service/plan?technology=${technology}&sgbd=${dbms}`,
+    url: `/api/v1/service/plan?technology=${technology}&sgbd=${dbms}`,
+  });
+
+export const fetchGPTCode = (
+  technology: string,
+  dbms: string,
+  feature: string
+) =>
+  instance({
+    url: `/api/v1/service/code?functionality=${feature}&technology=${technology}&sgbd=${dbms}`,
   });
