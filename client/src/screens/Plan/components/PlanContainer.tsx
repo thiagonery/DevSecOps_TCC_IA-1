d4515eb02862dffd9219282ef176a14f50d218b6
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-03-27 10:19:06 -0300

    feat: create request status

--- client/src/screens/Plan/components/PlanContainer.tsx
+++ client/src/screens/Plan/components/PlanContainer.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import { Status } from '@common/types';
 import { Box, Button, Input, Text } from '@components';
 import { useTheme } from '@mui/material';
 import { fetchGPTPlan } from '@services/gpt';
@@ -9,11 +10,20 @@ export const PlanContainer = () => {
   const [techValue, setTechValue] = useState('');
   const [dataBaseValue, setDataBaseValue] = useState('');
   const [gptResponse, setGptResponse] = useState('');
+  const [status, setStatus] = useState<Status>('idle');
 
   const handlefetchGPTPlan = () => {
+    setStatus('pending');
+
     fetchGPTPlan(techValue, dataBaseValue)
-      .then((res) => setGptResponse(res.data.content))
-      .catch((err) => console.log(err.response.data));
+      .then((res) => {
+        setStatus('succeeded');
+        setGptResponse(res.data.content);
+      })
+      .catch((err) => {
+        setStatus('failed');
+        console.log(err.response.data);
+      });
   };
 
   return (
@@ -40,17 +50,26 @@ export const PlanContainer = () => {
           label="Banco de dados"
         />
       </Box>
-      <Button text="Confirmar" onClick={handlefetchGPTPlan} />
+      <Button
+        text={status === 'pending' ? 'Carregando...' : 'Confirmar'}
+        disabled={status === 'succeeded' || status === 'pending'}
+        onClick={handlefetchGPTPlan}
+      />
       <>
         {gptResponse && (
           <>
             <Text fontSize="18" fontWeight="600" fontFamily="Titillium Web">
               {`Possíveis vulnerabilidades para "${techValue}", com banco de dados "${dataBaseValue}":`}
             </Text>
-            <Text fontSize="18" fontWeight="600" fontFamily="Titillium Web">
+            <Text fontSize="22px" color={theme.palette.primary.main}>
               {gptResponse}
             </Text>
           </>
+        )}
+        {status === 'failed' && (
+          <Text fontSize="22px" color={theme.palette.primary.main}>
+            Falha ao carregar resposta. Por favor, tente novamente mais tarde.
+          </Text>
         )}
       </>
     </Box>
