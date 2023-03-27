20de48d2491ba8fe4845e23990110fb0ac6b81ca
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-03-27 09:48:54 -0300

    chore: add conditional to run development server

--- server/app.py
+++ server/app.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask_cors import CORS
 from service.service import service 
+from waitress import serve
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 app.register_blueprint(service)
 CORS(app)
 
+load_dotenv()
 
 @app.route('/')
 def home():
     return 'Server is up! wellcome'
 
-
-if(__name__ == "__main__"):
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=3001)
-    
+if(__name__ == '__main__'):
+    if os.environ['ENV_MODE'] == 'production':
+        serve(app, host='0.0.0.0', port=3001)
+    else: 
+        app.run(debug=True)
+        
