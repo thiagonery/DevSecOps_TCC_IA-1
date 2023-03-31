013d2aeb998f978c10e0b9f0e8656986d2d1d74f
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-03-30 23:32:55 -0300

    chore: create fetchGPTRelease

--- client/src/services/gpt/index.ts
+++ client/src/services/gpt/index.ts
@@ -26,3 +26,8 @@ export const fetchGPTDeploy = (technology: string, dbms: string) =>
   instance({
     url: `/api/v1/service/deploy?technology=${technology}&sgbd=${dbms}`,
   });
+
+export const fetchGPTRelease = (technology: string, dbms: string) =>
+  instance({
+    url: `/api/v1/service/release?technology=${technology}&sgbd=${dbms}`,
+  });
