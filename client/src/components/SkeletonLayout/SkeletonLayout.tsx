8004cef890017bcee827e3a9aab086e777067870
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-04-04 23:51:17 -0300

    feat: create SkeletonLayout component

---
+++ client/src/components/SkeletonLayout/SkeletonLayout.tsx
@@ -0,0 +1,59 @@
+import { DrawerScreens, Status } from '@common/types';
+import { useTheme } from '@mui/material';
+
+import { Background } from '../Layout/Background';
+import { Box } from '../Layout/Box';
+import { Text } from '../Text';
+
+interface ISkeletonLayout {
+  drawerFocus: DrawerScreens;
+  title: string;
+  subtitle: string;
+  responseIntro: string;
+  gptResponse: string;
+  status: Status;
+  children: React.ReactElement | React.ReactElement[];
+}
+
+export const SkeletonLayout: React.FC<ISkeletonLayout> = ({
+  drawerFocus,
+  title,
+  subtitle,
+  responseIntro,
+  gptResponse,
+  status,
+  children,
+}) => {
+  const theme = useTheme();
+
+  return (
+    <Background drawerFocus={drawerFocus}>
+      <Box gap="24px">
+        <Text variant="h1" color={theme.palette.primary.main}>
+          {title}
+        </Text>
+        <Text fontSize="22px" color={theme.palette.primary.main}>
+          {subtitle}
+        </Text>
+        <>
+          {children}
+          {gptResponse && (
+            <>
+              <Text fontSize="18" fontWeight="600" fontFamily="Titillium Web">
+                {responseIntro}
+              </Text>
+              <Text fontSize="22px" color={theme.palette.primary.main}>
+                {gptResponse}
+              </Text>
+            </>
+          )}
+          {status === 'failed' && (
+            <Text fontSize="22px" color={theme.palette.primary.main}>
+              Falha ao carregar resposta. Por favor, tente novamente mais tarde.
+            </Text>
+          )}
+        </>
+      </Box>
+    </Background>
+  );
+};
