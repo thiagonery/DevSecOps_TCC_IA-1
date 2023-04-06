909569efbf67bd85fba4d55ca3562f42e8f6d9cc
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-04-05 22:45:04 -0300

    refactor: add SkeletonLayout in MonitorScreen

--- client/src/screens/Monitor/MonitorScreen.tsx
+++ client/src/screens/Monitor/MonitorScreen.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import { useState } from 'react';
 
 import { Status } from '@common/types';
-import { Background } from '@components';
-import { Box, Button, Input, Text } from '@components';
-import { useTheme } from '@mui/material';
+import { SkeletonLayout } from '@components';
+import { Box, Button, Input } from '@components';
 import { fetchGPTMonitor } from '@services/gpt';
 
 export const MonitorScreen: React.FC = () => {
-  const theme = useTheme();
   const [techValue, setTechValue] = useState('');
   const [dataBaseValue, setDataBaseValue] = useState('');
-
   const [gptResponse, setGptResponse] = useState('');
   const [status, setStatus] = useState<Status>('idle');
 
@@ -31,56 +28,37 @@ export const MonitorScreen: React.FC = () => {
       });
   };
   return (
-    <Background drawerFocus="monitor">
-      <Box gap="24px">
-        <Text variant="h1" color={theme.palette.primary.main}>
-          Etapa - MONITOR
-        </Text>
-        <Text fontSize="22px" color={theme.palette.primary.main}>
-          Especifique as tecnologias na etapa de monitoramento da aplicação.
-        </Text>
-
-        <Box flexDirection="row" gap="18px">
-          <Input
-            value={techValue}
-            onChange={(e) => setTechValue(e.target.value)}
-            fullWidth
-            label="Tecnologia"
-          />
-          <Input
-            value={dataBaseValue}
-            onChange={(e) => setDataBaseValue(e.target.value)}
-            fullWidth
-            label="Banco de dados"
-          />
-        </Box>
-        <Button
-          text={status === 'pending' ? 'Carregando...' : 'Confirmar'}
-          disabled={
-            status === 'succeeded' ||
-            status === 'pending' ||
-            inputs.some((input) => input === '')
-          }
-          onClick={handleFetchGPTMonitor}
+    <SkeletonLayout
+      drawerFocus="monitor"
+      title="Etapa - MONITOR"
+      subtitle="Especifique as tecnologias na etapa de monitoramento da aplicação."
+      responseIntro={`Implantação de SIEM, utilizando "${techValue}" e "${dataBaseValue}":`}
+      gptResponse={gptResponse}
+      status={status}
+    >
+      <Box flexDirection="row" gap="18px">
+        <Input
+          value={techValue}
+          onChange={(e) => setTechValue(e.target.value)}
+          fullWidth
+          label="Tecnologia"
         />
-        <>
-          {gptResponse && (
-            <>
-              <Text fontSize="18" fontWeight="600" fontFamily="Titillium Web">
-                {`Implantação de SIEM, utilizando "${techValue}" e "${dataBaseValue}":`}
-              </Text>
-              <Text fontSize="22px" color={theme.palette.primary.main}>
-                {gptResponse}
-              </Text>
-            </>
-          )}
-          {status === 'failed' && (
-            <Text fontSize="22px" color={theme.palette.primary.main}>
-              Falha ao carregar resposta. Por favor, tente novamente mais tarde.
-            </Text>
-          )}
-        </>
+        <Input
+          value={dataBaseValue}
+          onChange={(e) => setDataBaseValue(e.target.value)}
+          fullWidth
+          label="Banco de dados"
+        />
       </Box>
-    </Background>
+      <Button
+        text={status === 'pending' ? 'Carregando...' : 'Confirmar'}
+        disabled={
+          status === 'succeeded' ||
+          status === 'pending' ||
+          inputs.some((input) => input === '')
+        }
+        onClick={handleFetchGPTMonitor}
+      />
+    </SkeletonLayout>
   );
 };
