946319822fb1bb2f9f0e4da46a7230a864098ce3
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-04-05 22:50:34 -0300

    refactor: add SkeletonLayout in TestScreen

--- client/src/screens/Test/TestScreen.tsx
+++ client/src/screens/Test/TestScreen.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { useState } from 'react';
 
 import { Status } from '@common/types';
-import { Background } from '@components';
-import { Box, Button, Input, Text } from '@components';
-import { useTheme } from '@mui/material';
+import { SkeletonLayout } from '@components';
+import { Box, Button, Input } from '@components';
 import { fetchGPTTest } from '@services/gpt';
 
 export const TestScreen: React.FC = () => {
-  const theme = useTheme();
   const [techValue, setTechValue] = useState('');
   const [dataBaseValue, setDataBaseValue] = useState('');
   const [gptResponse, setGptResponse] = useState('');
@@ -31,57 +29,38 @@ export const TestScreen: React.FC = () => {
   };
 
   return (
-    <Background drawerFocus="test">
-      <Box gap="24px">
-        <Text variant="h1" color={theme.palette.primary.main}>
-          Etapa - TEST
-        </Text>
-        <Text fontSize="22px" color={theme.palette.primary.main}>
-          Melhores práticas de teste para as tecnologias utilizadas na sua
-          aplicação.
-        </Text>
-
-        <Box flexDirection="row" gap="18px">
-          <Input
-            value={techValue}
-            onChange={(e) => setTechValue(e.target.value)}
-            fullWidth
-            label="Tecnologia"
-          />
-          <Input
-            value={dataBaseValue}
-            onChange={(e) => setDataBaseValue(e.target.value)}
-            fullWidth
-            label="Banco de dados"
-          />
-        </Box>
-        <Button
-          text={status === 'pending' ? 'Carregando...' : 'Confirmar'}
-          disabled={
-            status === 'succeeded' ||
-            status === 'pending' ||
-            inputs.some((input) => input === '')
-          }
-          onClick={handleFetchGPTTest}
+    <SkeletonLayout
+      drawerFocus="test"
+      title="Etapa - TEST"
+      subtitle="Melhores práticas de teste para as tecnologias utilizadas na sua
+      aplicação."
+      responseIntro={`DAST em uma aplicação "${techValue}" utilizando o banco de dados "${dataBaseValue}":`}
+      gptResponse={gptResponse}
+      status={status}
+    >
+      <Box flexDirection="row" gap="18px">
+        <Input
+          value={techValue}
+          onChange={(e) => setTechValue(e.target.value)}
+          fullWidth
+          label="Tecnologia"
         />
-        <>
-          {gptResponse && (
-            <>
-              <Text fontSize="18" fontWeight="600" fontFamily="Titillium Web">
-                {`DAST em uma aplicação "${techValue}" utilizando o banco de dados "${dataBaseValue}":`}
-              </Text>
-              <Text fontSize="22px" color={theme.palette.primary.main}>
-                {gptResponse}
-              </Text>
-            </>
-          )}
-          {status === 'failed' && (
-            <Text fontSize="22px" color={theme.palette.primary.main}>
-              Falha ao carregar resposta. Por favor, tente novamente mais tarde.
-            </Text>
-          )}
-        </>
+        <Input
+          value={dataBaseValue}
+          onChange={(e) => setDataBaseValue(e.target.value)}
+          fullWidth
+          label="Banco de dados"
+        />
       </Box>
-    </Background>
+      <Button
+        text={status === 'pending' ? 'Carregando...' : 'Confirmar'}
+        disabled={
+          status === 'succeeded' ||
+          status === 'pending' ||
+          inputs.some((input) => input === '')
+        }
+        onClick={handleFetchGPTTest}
+      />
+    </SkeletonLayout>
   );
 };
