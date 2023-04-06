9d23d4a64054d9f547a69bc375e47414ed385d78
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-04-05 22:41:41 -0300

    refactor: add SkeletonLayout in BuildScreen

--- client/src/screens/Build/BuildScreen.tsx
+++ client/src/screens/Build/BuildScreen.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { useState } from 'react';
 
 import { Status } from '@common/types';
-import { Background } from '@components';
-import { Box, Button, Input, Text } from '@components';
-import { useTheme } from '@mui/material';
+import { SkeletonLayout } from '@components';
+import { Box, Button, Input } from '@components';
 import { fetchGPTBuild } from '@services/gpt';
 
 export const BuildScreen: React.FC = () => {
-  const theme = useTheme();
   const [techValue, setTechValue] = useState('');
 
   const [gptResponse, setGptResponse] = useState('');
@@ -28,48 +26,29 @@ export const BuildScreen: React.FC = () => {
       });
   };
   return (
-    <Background drawerFocus="build">
-      <Box gap="24px">
-        <Text variant="h1" color={theme.palette.primary.main}>
-          Etapa - BUILD
-        </Text>
-        <Text fontSize="22px" color={theme.palette.primary.main}>
-          Especifique a tecnologia utilizada na etapa de build.
-        </Text>
-
-        <Box flexDirection="row" gap="18px">
-          <Input
-            value={techValue}
-            onChange={(e) => setTechValue(e.target.value)}
-            fullWidth
-            label="Tecnologia"
-          />
-        </Box>
-        <Button
-          text={status === 'pending' ? 'Carregando...' : 'Confirmar'}
-          disabled={
-            status === 'succeeded' || status === 'pending' || techValue === ''
-          }
-          onClick={handleFetchGPTBuild}
+    <SkeletonLayout
+      drawerFocus="build"
+      title="Etapa - BUILD"
+      subtitle="Especifique a tecnologia utilizada na etapa de build."
+      responseIntro={`Ferramentas de análise de build para "${techValue}" com foco na segurança da aplicação.`}
+      gptResponse={gptResponse}
+      status={status}
+    >
+      <Box flexDirection="row" gap="18px">
+        <Input
+          value={techValue}
+          onChange={(e) => setTechValue(e.target.value)}
+          fullWidth
+          label="Tecnologia"
         />
-        <>
-          {gptResponse && (
-            <>
-              <Text fontSize="18" fontWeight="600" fontFamily="Titillium Web">
-                {`Ferramentas de análise de build para "${techValue}" com foco na segurança da aplicação.`}
-              </Text>
-              <Text fontSize="22px" color={theme.palette.primary.main}>
-                {gptResponse}
-              </Text>
-            </>
-          )}
-          {status === 'failed' && (
-            <Text fontSize="22px" color={theme.palette.primary.main}>
-              Falha ao carregar resposta. Por favor, tente novamente mais tarde.
-            </Text>
-          )}
-        </>
       </Box>
-    </Background>
+      <Button
+        text={status === 'pending' ? 'Carregando...' : 'Confirmar'}
+        disabled={
+          status === 'succeeded' || status === 'pending' || techValue === ''
+        }
+        onClick={handleFetchGPTBuild}
+      />
+    </SkeletonLayout>
   );
 };
