d516865ba24c22724f12711f572edb96e53ab585
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-04-13 23:43:33 -0300

    style: remove explicit return in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,8 @@ import './App.css';
 import { Routes } from './routes';
 import { defaultTheme } from './themes/default';
 
-export const App = () => {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Routes />
-    </ThemeProvider>
-  );
-};
+export const App = () => (
+  <ThemeProvider theme={defaultTheme}>
+    <Routes />
+  </ThemeProvider>
+);
