9775cf12987a56c3a9bbd2d212389cba2907be48
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-05-10 00:03:40 -0300

    feat: change theme to darkTheme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from '@mui/material';
+import { darkTheme } from '@themes/dark';
 
 import './App.css';
 import { Routes } from './routes';
-import { defaultTheme } from '@themes/default';
 
 export const App = () => (
-  <ThemeProvider theme={defaultTheme}>
+  <ThemeProvider theme={darkTheme}>
     <Routes />
   </ThemeProvider>
 );
